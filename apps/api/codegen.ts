e862965788f131a6502ac5370e0427a21cc1b26d
mrae43 <khaelsm.mrae43@hmail.com>
2026-03-20 14:23:19 +0800

    feat (api): configure plugin create codegen.ts file

---
+++ apps/api/codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:3000/graphql", 
+  generates: {
+    "src/graphql/__generated__/types.ts": {
+      plugins: ["typescript", "typescript-resolvers"],
+      config: {
+        contextType: "../../types/context#MyContext",
+        useIndexSignature: true,
+        maybeValue: 'T | null | undefined',
+        mappers: {
+          Sentence: '../models/sentences#SentenceDocument',
+        },
+      },
+    },
+  },
+};
+
+export default config;
