ca5d55522d57f45ca2ee5f08eccca851e969c141
mrae43 <khaelsm.mrae43@hmail.com>
2026-03-24 14:22:49 +0800

    fix (api): update codegen condiguration matching the new architecture schema

--- apps/api/codegen.ts
+++ apps/api/codegen.ts
@@ -11,7 +11,7 @@ const config: CodegenConfig = {
         useIndexSignature: true,
         maybeValue: 'T | null | undefined',
         mappers: {
-          Sentence: '../../models/sentence#SentenceDocument',
+          ThoughtNode: '../../models/sentence#ThoughtNodeDocument',
         },
       },
     },
